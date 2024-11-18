a907e44259f21017fa37d96263181e5677bb9bfb
Mohamedxzxzmm <momexprs888@gmail.com>
2024-11-18 12:52:23 +0200

    ᎠᎬᏕᎨᎶᏁᎬᎠ ᏰᎽ ᎲᎾᏕ ᎿᎬᎯᎷ

--- plugins/Ai•gemini.js
+++ plugins/Ai•gemini.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 var handler = async (m, { text,  usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `『🪐』𝙄𝙣𝙜𝙧𝙚𝙨𝙚 𝙪𝙣 𝙩𝙚𝙭𝙩𝙤 𝙥𝙖𝙧𝙖 𝙪𝙨𝙖𝙧 𝙚𝙨𝙩𝙚 𝙘𝙤𝙢𝙖𝙣𝙙𝙤.\n\n• 𝙋𝙤𝙧 𝙚𝙟𝙚𝙢𝙥𝙡𝙤:\n${usedPrefix + command} Hola`, m, rcanal)
+if (!text) return conn.reply(m.chat, `『🪐』ادخل نص لاستخدام الأمر.\n\n• مثال:\n${usedPrefix + command} مرحبا`, m, rcanal)
 try {
 await m.react(rwait)
 conn.sendPresenceUpdate('composing', m.chat)
@@ -9,9 +9,9 @@ var res = await apii.json()
 await m.reply(res.result)
 } catch {
 await m.react(error)
-await conn.reply(m.chat, `『📣』𝙊𝙘𝙪𝙧𝙧𝙞𝙤 𝙪𝙣 𝙚𝙧𝙧𝙤𝙧 𝙚𝙣 𝙚𝙡 𝙘𝙤𝙢𝙖𝙣𝙙𝙤, 𝙧𝙚𝙥𝙤𝙧𝙩𝙖𝙡𝙤 𝙖𝙡 𝙘𝙧𝙚𝙖𝙙𝙤𝙧 𝙙𝙚𝙡 𝙗𝙤𝙩.`, m, rcanal)
+await conn.reply(m.chat, `『📣』حدث خطأ غير متوقع.`, m, rcanal)
 }}
-handler.command = ['gemini']
+handler.command = ['جيم']
 handler.help = ['gemini']
 handler.tags = ['ai']
 
